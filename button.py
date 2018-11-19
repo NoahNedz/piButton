eec94131feb7b4520daae0ca04ad7ceec1f119ed
Noah <noahnedz@live.com>
2018-11-19 13:44:59 -0600

    removed print message

--- button.py
+++ button.py
@@ -1,3 +1,7 @@
+#Noah Niedzwiecki | 2018 Nov. 15
+#Must have RPi.GPIO installed to run
+#This code is meant for a raspberry pi running python v-2.7 or higher
+
 import RPi.GPIO as GPIO
 import time
 from time import sleep
@@ -7,7 +11,7 @@ readPin = 14
 ledPin = 15
 print(GPIO.getmode())
 GPIO.setmode(GPIO.BCM)
-print(GPIO.getmode())
+
 
 GPIO.setup(ledPin, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(readPin, GPIO.IN)
@@ -16,29 +20,29 @@ status = "off"
 
 try:
 	while 1:
-		print("current state is " + status)
+		
 		if status == "off":
 			if GPIO.input(readPin):
 				GPIO.output(ledPin, GPIO.HIGH)
 				status = "on"
-				print("button pressed")
+				
 				time.sleep(5)
 			continue
 		else:
 			if GPIO.input(readPin):
-				print("initial press")
+				
 				time.sleep(2)
-				print("just slept")
+				
 				if GPIO.input(readPin):
 					status = "off"
-					print("status " + status)
-					print("continued to wait, setting low")
+					
+					
 					GPIO.output(ledPin, GPIO.LOW)
 					time.sleep(5)
 				else:
-					print("wasnt held down")
+					
 					time.sleep(1)
-					print("status " + status)
+					
 except KeyboardInterrupt:
 	GPIO.output(ledPin, GPIO.LOW)
 	GPIO.cleanup()
